59c5d63a4cfc270e4fe24a83aac0055f765f7e43
KimGatwiri <gatwirifloridah4@gmail.com>
2026-05-25 15:21:13 +0300

    Chrome extension

---
+++ index.js
@@ -0,0 +1,58 @@
+myLeads=[]
+const inputEl=document.getElementById("input-el")
+const  inputBtn=document.getElementById("input-btn")
+const deleteBtn=document.getElementById("delete-btn")
+const ulEl=document.getElementById("ul-el")
+const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+const tabBtn=document.getElementById("tab-btn")
+if(leadsFromLocalStorage){
+    myLeads=leadsFromLocalStorage
+    render(myLeads)
+}
+const tabs=[{
+    url:"https://www.linkedin.com/in/per-harald-borgen"
+}]
+
+tabBtn.addEventListener("click",function(){
+    
+    chrome.tabs.query({active:true,currentWindow:true},function(tabs){
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads",JSON.stringify(myLeads))
+    render(myLeads)
+    })
+    
+    
+})
+
+function render(Leads){
+let listItems=""
+
+for(let i=0;i<Leads.length;i++){
+       
+       listItems+=`
+            <li>
+                <a target='_blank' href='${Leads[i]}'>
+                    ${Leads[i]}
+                </a>
+            </li>
+        `
+}
+ulEl.innerHTML= listItems
+}
+
+inputBtn.addEventListener("click",function(){
+    myLeads.push(inputEl.value)
+    inputEl.value=""
+    localStorage.setItem("myLeads",JSON.stringify(myLeads) )
+    
+    
+    render(myLeads)
+    console.log(localStorage.getItem("myLeads"))
+    
+})
+
+deleteBtn.addEventListener("dblclick",function(){
+    localStorage.clear()
+    myLeads=[]
+    render(myLeads)
+})
