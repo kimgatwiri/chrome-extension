6dd3aadca746a7d0c4a8f37aa060313e70de3715
KimGatwiri <gatwirifloridah4@gmail.com>
2026-05-28 11:32:24 +0300

    removed all localstorage data

--- index.js
+++ index.js
@@ -14,14 +14,11 @@ const inputEl=document.getElementById("input-el")
 const  inputBtn=document.getElementById("input-btn")
 const deleteBtn=document.getElementById("delete-btn")
 const ulEl=document.getElementById("ul-el")
-const leadsFromLocalStorage=JSON.parse(localStorage.getItem("myLeads"))
+
 const tabBtn=document.getElementById("tab-btn")
 
 
-if(leadsFromLocalStorage){
-    myLeads=leadsFromLocalStorage
-    render(myLeads)
-}
+
 
 
 // tabBtn.addEventListener("click",function(){
@@ -54,16 +51,16 @@ ulEl.innerHTML= listItems
 inputBtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
     inputEl.value=""
-    localStorage.setItem("myLeads",JSON.stringify(myLeads) )
+    
     
     
     render(myLeads)
-    console.log(localStorage.getItem("myLeads"))
+   
     
 })
 
 deleteBtn.addEventListener("dblclick",function(){
-    localStorage.clear()
+    
     myLeads=[]
     render(myLeads)
 })
